a61a79c912d5985c4da175daf02a72bdc65dd597
06Pranay <134846912+06Pranay@users.noreply.github.com>
2024-06-02 18:01:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 export const connect = async()=>{
-    await mongoose.connect("mongodb://127.0.0.1:27017/chatApp", {
+    await mongoose.connect("mongodb://localhost:27017/chatApp", {
         useNewUrlParser: true,
         useUnifiedTopology: true
     });
